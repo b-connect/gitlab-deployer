54b3ad9c2251fa655fc5db2d3968f1f92a4bf35d
Erik Seifert <erik.seifert@b-connect.de>
2017-11-28 17:43:32 +0100

    Update

--- runner.sh
+++ runner.sh
@@ -3,4 +3,4 @@ echo "$SSH_KEY" > /root/.ssh/id_rsa
 chmod 700 /root/.ssh/id_rsa
 cd /
 export PATH="%PATH:/vendor/bin:/usr/local/bin:/usr/bin"
-dep deploy --tag="${CI_COMMIT_TAG}"
+script "dep deploy --tag=${CI_COMMIT_TAG}"
