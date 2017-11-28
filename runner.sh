b4d65403cabbb171eef7bb60586fc100f6908a4f
Erik Seifert <erik.seifert@b-connect.de>
2017-11-29 00:06:07 +0100

    Update

--- runner.sh
+++ runner.sh
@@ -6,4 +6,9 @@ export PATH="%PATH:/vendor/bin:/usr/local/bin:/usr/bin"
 touch log
 # tail -f log &
 # script -e -c "dep deploy --tag=${CI_COMMIT_TAG}" log
-dep deploy
+if [[ -z "$CI_COMMIT_TAG" ]]
+  then
+    dep deploy --tag="${CI_COMMIT_TAG}"
+  else
+    dep deploy --revision=${CI_COMMiT_SHA}
+fi;
