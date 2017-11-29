90f4908696674f31c9bbe8a2e628c2b04cfce85a
Erik Seifert <erik.seifert@b-connect.de>
2017-11-29 01:33:34 +0100

    Update

--- runner.sh
+++ runner.sh
@@ -8,7 +8,7 @@ echo "DEPLOY TO  ${CI_ENVIRONMENT_URL}"
 
 if [[ -z "$CI_COMMIT_TAG" ]]
   then
-    dep deploy --tag="${CI_COMMIT_TAG}"
-  else
     dep deploy --revision=${CI_COMMiT_SHA}
+  else
+    dep deploy --tag="${CI_COMMIT_TAG}"
 fi;
